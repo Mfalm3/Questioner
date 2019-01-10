52b41ee6f0e12f6c289353022c4148ebb7cd1f7a
Joe Waithaka <waitha28ka@gmail.com>
2019-01-10 11:36:19 +0300

    [Start #163048484] Initialize the questions model

---
+++ app/api/v1/models/questions_model.py
@@ -0,0 +1,35 @@
+"""Question Model."""
+import datetime
+from flask import jsonify
+from .base_model import BaseModel
+from app.db import init_db, question_db
+
+
+class QuestionModel(BaseModel):
+    """Question model Tests class."""
+
+    def __init__(self):
+        """Initialize the Meetup model."""
+        self.db = init_db(question_db)
+
+    def question(self, title, body, meetup, author, votes=0):
+        """Question object."""
+        query = {
+            "id": len(self.db) + 1,
+            "title": title,
+            "body": body,
+            "meetup": meetup,
+            "createdOn": datetime.datetime.now().strftime("%I:%M:%S%P %d %b %Y"),
+            "createdBy": author,
+            "votes": votes,
+        }
+        return query
+
+    def save(self, question):
+        """Store a question method."""
+        db = self.db
+        db.append(question)
+        return jsonify({
+            "status": 201,
+            "message": "Question posted successfully!"
+        })
