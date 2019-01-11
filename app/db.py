a3ccf3651fd7121ed6c311a3623188b34a1f1a14
Joe Waithaka <waitha28ka@gmail.com>
2019-01-11 13:59:15 +0300

    [Bug #163145127] Remove dummy data

--- app/db.py
+++ app/db.py
@@ -1,22 +1,8 @@
 
 db = []
 user_db = []
-meetup_db = [{
-    "id": 1,
-    "title": "Catching up with the world of JS",
-    "location": "KCB sports club",
-    "happeningOn": "10:00am 25 Jan 2019",
-    "tags": ["VueJs", "ES2016"]
-}]
-question_db = [{
-    "id": 1,
-    "title": "Catching up with the world of JS",
-    "body": "An exploration of the upcoming things in javascript trends",
-    "meetup": 1,
-    "createdOn": "12:12:43pm 9 Jan 2019",
-    "createdBy": "waithaka",
-    "votes": 0,
-}]
+meetup_db = []
+question_db = []
 comment_db = []
 test_db = []
 
