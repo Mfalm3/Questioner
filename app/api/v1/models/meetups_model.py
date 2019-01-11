c2e62bfdf8dd24666ec87928cb6baae2c4d22c17
Joe Waithaka <waitha28ka@gmail.com>
2019-01-11 11:18:52 +0300

    [Start #163048390] Write code for instantiating and saving a new meetup

--- app/api/v1/models/meetups_model.py
+++ app/api/v1/models/meetups_model.py
@@ -1,4 +1,5 @@
 """Meetups Model file."""
+import datetime
 from .base_model import BaseModel, init_db
 from app.db import meetup_db
 
@@ -9,6 +10,24 @@ class MeetupsModel(BaseModel):
     def __init__(self):
         """Initialize the Meetup model."""
         self.db = init_db(meetup_db)
+
+    def meetup(self, location, images, topic, happeningOn, tags):
+        """Meetup object."""
+        meetup = {
+                    "id": len(self.db) + 1,
+                    "createdOn": datetime.datetime.now().strftime("%I:%M:%S%P %d %b %Y"),
+                    "location": location,
+                    "images": images,
+                    "topic": topic,
+                    "happeningOn": happeningOn,
+                    "tags": tags,
+                    }
+        return meetup
+
+    def create(self, meetup):
+        """Create a new meetup."""
+        return self.db.append(meetup)
+
 
     def get_all(self):
         """Get all meetups."""
