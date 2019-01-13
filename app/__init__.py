a2fd2c20578526caab4bdcb8ff4bd7e9f4289fa4
Joe Waithaka <waitha28ka@gmail.com>
2019-01-13 13:52:24 +0300

    [Chore #163176756] Register question blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from app.api.v1.views.api_views import v1
 from app.api.v1.views.meetup_views import v1_m
+from app.api.v1.views.questions_view import v1_q
 from instance.config import app_config
 
 
@@ -9,6 +10,7 @@ def create_app(config='development'):
     app = Flask(__name__)
     app.register_blueprint(v1, url_prefix='/api/v1')
     app.register_blueprint(v1_m, url_prefix='/api/v1')
+    app.register_blueprint(v1_q, url_prefix='/api/v1')
     app.config.from_object(app_config[config])
     app.config.from_pyfile('../instance/config.py')
 
