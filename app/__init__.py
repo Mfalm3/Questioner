e92d69d40be86e1cfeb2080a44d4bca7a07f366f
Joe Waithaka <waitha28ka@gmail.com>
2019-01-13 15:50:01 +0300

    [Chore #163176756] Refactor blueprint imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,12 @@
 """Initializing the app."""
 from flask import Flask
-from app.api.v1.views.api_views import v1
-from app.api.v1.views.meetup_views import v1_m
-from app.api.v1.views.questions_view import v1_q
+from app.api.v1.views.api_views import v1_u, v1_m, v1_q
 from instance.config import app_config
 
 
 def create_app(config='development'):
     app = Flask(__name__)
-    app.register_blueprint(v1, url_prefix='/api/v1')
+    app.register_blueprint(v1_u, url_prefix='/api/v1')
     app.register_blueprint(v1_m, url_prefix='/api/v1')
     app.register_blueprint(v1_q, url_prefix='/api/v1')
     app.config.from_object(app_config[config])
