8e5ee96f4460fe34a0ddbad5682ce8325834ffcb
Joe Waithaka <waitha28ka@gmail.com>
2019-01-14 23:56:06 +0300

    [Chore #163204417] Refactor the docstrings

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
-"""Initializing the app."""
+# Initializing the app.
 from flask import Flask
 from app.api.v1.views.api_views import v1_users, v1_meetups, v1_questions
 from instance.config import app_config
 
 
 def create_app(config='development'):
+    """Initialize the app function."""
     app = Flask(__name__)
     app.register_blueprint(v1_users, url_prefix='/api/v1')
     app.register_blueprint(v1_meetups, url_prefix='/api/v1')
