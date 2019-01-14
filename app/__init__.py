5a9f993d94b7995132665d7e5ac2fa9cedf82628
Joe Waithaka <waitha28ka@gmail.com>
2019-01-14 23:46:00 +0300

    [Chore #163204417] Refactor meetups variable names

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,14 @@
 """Initializing the app."""
 from flask import Flask
-from app.api.v1.views.api_views import v1_u, v1_m, v1_q
+from app.api.v1.views.api_views import v1_users, v1_meetups, v1_questions
 from instance.config import app_config
 
 
 def create_app(config='development'):
     app = Flask(__name__)
-    app.register_blueprint(v1_u, url_prefix='/api/v1')
-    app.register_blueprint(v1_m, url_prefix='/api/v1')
-    app.register_blueprint(v1_q, url_prefix='/api/v1')
+    app.register_blueprint(v1_users, url_prefix='/api/v1')
+    app.register_blueprint(v1_meetups, url_prefix='/api/v1')
+    app.register_blueprint(v1_questions, url_prefix='/api/v1')
     app.config.from_object(app_config[config])
     app.config.from_pyfile('../instance/config.py')
 
