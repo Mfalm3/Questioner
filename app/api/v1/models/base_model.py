c4da7d26b31d354de02072df7e91bdb801603b0c
Joe Waithaka <waitha28ka@gmail.com>
2019-01-15 00:09:43 +0300

    [Chore #163204417] Add docstring

--- app/api/v1/models/base_model.py
+++ app/api/v1/models/base_model.py
@@ -1,8 +1,9 @@
-"""Base model class"""
+# Base model class
 
 from app.db import init_db
 
 
 class BaseModel(object):
+    """Base class model with bare shared across varous files"""
     def __init__(self):
         self.db = init_db()
