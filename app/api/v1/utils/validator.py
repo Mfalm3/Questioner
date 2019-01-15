c2a3f0163436cb8224c22a7116f863b0ae9c2c52
Joe Waithaka <waitha28ka@gmail.com>
2019-01-15 11:18:06 +0300

    [Chore #163204417] Add validations

--- app/api/v1/utils/validator.py
+++ app/api/v1/utils/validator.py
@@ -8,6 +8,7 @@ def valid_email(email):
         return True
     return False
 
+
 def email_exists(email, db):
     """Check if email exists in database."""
     if any(row['email'] == email for row in db):
@@ -19,5 +20,24 @@ def username_exists(username, db):
     """Check if username exists in database."""
     for rows in db:
         if username in rows.values():
+            return True
+        return False
+
+
+def is_empty(*args):
+    """Check for empty strings"""
+    items = [*args]
+    for item in items:
+        check_string = item.replace(" ", "").strip()
+        if check_string == "":
+            return True
+        return False
+
+
+def no_numbers(string):
+    """Check for numbers in string"""
+    if is_empty(string) is False:
+        check_string = string.strip().replace(' ', '')
+        if check_string.isalpha():
             return True
         return False
