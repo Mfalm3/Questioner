54de49e9b6a09245b88296d72a2d67510f5b3f97
Joe Waithaka <waitha28ka@gmail.com>
2019-01-16 20:21:06 +0300

    [Chore #163204417] Refactor to remove multiple return statements

--- app/api/v1/views/meetup_views.py
+++ app/api/v1/views/meetup_views.py
@@ -21,33 +21,30 @@ def create_meetup(user):
     try:
         data = request.get_json()
 
+        for key, value in data.items():
+            if isinstance(value, str):
+                if is_empty(value):
+                    return jsonify({
+                        "status": 400,
+                        "message": "{} is missing.".format(key)
+                    }), 400
+                if isinstance(value, list):
+                    values_list = ','.join(item for item in value)
+                    values_list = values_list.split(',')
+                else:
+                    value = value.split(',')
+
         topic = data.get('topic')
         location = data.get('location')
         images = data.get('images')
-        happeningOn = data.get('happeningOn')
+        happening_on = data.get('happeningOn')
         tags = data.get('tags')
 
-        if isinstance(tags, list):
-            tag = ','.join(item for item in tags)
-            tag = tag.split(',')
-        else:
-            tag = tags.split(',')
-
-        if not topic or is_empty(topic):
-            return jsonify({
-                "status": 400,
-                "message": "{} is missing.".format("topic")
-            }), 400
         topic_length = required_length(topic, "topic", 10)
         if topic_length is not True:
             return jsonify({
                 "status": 400,
                 "error": topic_length
-            }), 400
-        if not location:
-            return jsonify({
-                "status": 400,
-                "message": "{} is missing.".format("location")
             }), 400
         location_length = required_length(location, "location", 10)
         if location_length is not True:
@@ -55,20 +52,10 @@ def create_meetup(user):
                 "status": 400,
                 "error": location_length
             }), 400
-        if not happeningOn:
-            return jsonify({
-                "status": 400,
-                "message": "{} is missing.".format("happeningOn")
-            }), 400
-        if not tags:
-            return jsonify({
-                "status": 400,
-                "message": "{} is missing.".format("tags")
-            }), 400
 
         new_meetup = the_meetup.meetup(location=location,
                                        images=images, topic=topic,
-                                       happeningOn=happeningOn, tags=tag)
+                                       happeningOn=happening_on, tags=tags)
         the_meetup.create(new_meetup)
         return jsonify({
             "status": 201,
@@ -76,8 +63,8 @@ def create_meetup(user):
             "data": [{
                 "topic": topic,
                 "location": location,
-                "happeningOn": happeningOn,
-                "tags": tag
+                "happeningOn": happening_on,
+                "tags": tags
                         }]
         }), 201
     except Exception as e:
@@ -145,7 +132,7 @@ def rsvp_a_meetup(user, meetup_id):
             new_rsvp = the_meetup.rsvp(meetup=meetup, user=user, response=resp)
             return the_meetup.create_rsvp(rsvp=new_rsvp)
 
-    except Exception:
+    except Exception as e:
         return jsonify({
             "status": 404,
             "error": "The meetup of the given id is not found"
