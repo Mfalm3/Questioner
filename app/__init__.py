9184640af17fd77ea7dc4fd7dd50fc76f5177e8e
Joe Waithaka <waitha28ka@gmail.com>
2019-01-17 13:30:57 +0300

    [Chore #163251587] Register v2 blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 # Initializing the app.
 from flask import Flask, jsonify
 from app.api.v1.views.api_views import v1_users, v1_meetups, v1_questions
+from app.api.v2.views.api_views import v2_user_bp, v2_meetup_bp, v2_question_bp
 from instance.config import app_config
 
 
@@ -34,12 +35,17 @@ def create_app(config='development'):
     """Initialize the app function."""
     app = Flask(__name__)
     app.url_map.strict_slashes = False
+    app.config.from_object(app_config[config])
+    app.config.from_pyfile('../instance/config.py')
+    # v1 blueprints
     app.register_blueprint(v1_users, url_prefix='/api/v1')
     app.register_blueprint(v1_meetups, url_prefix='/api/v1')
     app.register_blueprint(v1_questions, url_prefix='/api/v1')
-    app.config.from_object(app_config[config])
-    app.config.from_pyfile('../instance/config.py')
-
+    # v2 blueprints
+    app.register_blueprint(v2_user_bp, url_prefix='/api/v2')
+    app.register_blueprint(v2_meetup_bp, url_prefix='/api/v2')
+    app.register_blueprint(v2_question_bp, url_prefix='/api/v2')
+    # Register error handlers
     app.register_error_handler(405, method_not_allowed)
     app.register_error_handler(404, resource_not_found)
     app.register_error_handler(500, internal_server_error)
