3ccce8e9ee25a319395bc8510c4770ece5109ce8
Joe Waithaka <waitha28ka@gmail.com>
2019-01-17 12:50:09 +0300

    [Chore #163251587] Refactor variable name

--- app/api/v2/views/questions_views.py
+++ app/api/v2/views/questions_views.py
@@ -1,5 +1,5 @@
 # Question Views v2.
 from flask import Blueprint
 
-v2_question_blueprint = Blueprint('v2_question_blueprint',
+V2_QUESTION_BLUEPRINT = Blueprint('v2_question_blueprint',
                                   __name__, url_prefix='/api/v2')
