4b0d68b5f0fe0698a33a0e605fe4aa9cd59baeef
Joe Waithaka <waitha28ka@gmail.com>
2019-01-17 14:18:23 +0300

    [Chore #163251587] Refactor module imports

--- app/api/v2/views/api_views.py
+++ app/api/v2/views/api_views.py
@@ -1,7 +1,7 @@
 # Api v2 views
-from app.meetup_views import V2_MEETUP_BLUEPRINT
-from .questions_views import V2_QUESTION_BLUEPRINT
-from .user_views import V2_USER_BLUEPRINT
+from app.api.v2.views.meetup_views import V2_MEETUP_BLUEPRINT
+from app.api.v2.views.questions_views import V2_QUESTION_BLUEPRINT
+from app.api.v2.views.user_views import V2_USER_BLUEPRINT
 
 v2_meetup_bp = V2_MEETUP_BLUEPRINT
 v2_user_bp = V2_USER_BLUEPRINT
