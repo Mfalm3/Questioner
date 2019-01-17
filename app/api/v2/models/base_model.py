efb9253e2be2a8e2db5e9134ad066e1d5edeb40c
Joe Waithaka <waitha28ka@gmail.com>
2019-01-17 11:03:35 +0300

    [chore #163251587] Initialize base model file

---
+++ app/api/v2/models/base_model.py
@@ -0,0 +1,11 @@
+""" Base model class v2"""
+
+from app.db import init_dbase
+
+
+class BaseModel(object):
+    """Base class model with bare shared across varous files"""
+
+    def __init__(self):
+        """Initialize the base model class"""
+        self.db = init_dbase()
