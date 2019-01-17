385230f6be75176b54c03eae875f0176054a354a
Joe Waithaka <waitha28ka@gmail.com>
2019-01-17 11:13:57 +0300

    [Chore #163251587] Initialize the question model

---
+++ app/api/v2/models/questions_model.py
@@ -0,0 +1,11 @@
+""" Question Model v2."""
+from app.api.v1.models.base_model import BaseModel
+
+
+class QuestionModel(BaseModel):
+    """Question model Tests class."""
+
+    def __init__(self):
+        """Initialize the Meetup model."""
+
+        super(QuestionModel, self).__init__()
