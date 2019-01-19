9c8e9e3d2931e2db499268f0e1cb43f506e79f97
Joe Waithaka <waitha28ka@gmail.com>
2019-01-19 22:22:08 +0300

    [Chore #163204417] Refactor to add condition for voting once

--- app/api/v1/models/questions_model.py
+++ app/api/v1/models/questions_model.py
@@ -44,22 +44,22 @@ class QuestionModel(BaseModel):
         query = self.db[question_id - 1]
         return query
 
-    def upvote(self,user, question):
+    def upvote(self, user, question):
         """Upvote a question method."""
         query = question
-        if query.get('user') is None:
+        if query.get('user') is None or query['user'].get('hasvoted') == 1 and query['user'].get('id') != user['email']:
             query['votes'] += 1
-            query.update({'user':{'id':user['id'], 'hasvoted': 1}})
-        elif query['user'].get('hasvoted') == 1:
+            query.update({'user':{'id':user['email'], 'hasvoted': 1}})
+        elif query['user'].get('hasvoted') == 1 and query['user'].get('id') == user['email']:
             return False
         return query
 
     def downvote(self, user, question):
         """Downvote a question method."""
         query = question
-        if query.get('user') is None:
+        if query.get('user') is None or query['user'].get('hasvoted') == 1 and query['user'].get('id') != user['email']:
             query['votes'] -= 1
-            query.update({'user':{'id':user['id'], 'hasvoted': 1}})
+            query.update({'user':{'id':user['email'], 'hasvoted': 1}})
         elif query['user'].get('hasvoted') == 1:
             return False
         return query
