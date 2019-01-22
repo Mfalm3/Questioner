34cc5bff96b7b2f81ae5a1332c2c4c5c9557bf67
Joe Waithaka <waitha28ka@gmail.com>
2019-01-22 13:52:24 +0300

    [Feature #163347505] Refactor to include repetitve test. Add login tests

--- app/tests/v2/test_users.py
+++ app/tests/v2/test_users.py
@@ -7,25 +7,19 @@ class TestUsers(BaseTest):
     """Users Auth test class"""
     def test_signup(self):
         """Test for user signup"""
-        with self.client as c:
-            headers = {"Content-type": self.mime_type}
-            response = c.post(self.signup_url,
-                              json=self.signup_payload0,
-                              headers=headers)
-            result = json.loads(response.data.decode('utf-8'))
+        result = self.sign_up()
 
-            self.assertEqual(result['message'],
-                             "User `waithaka` created successfully!")
+        self.assertEqual(result['message'],
+                         "User `waithaka` created successfully!")
 
     def test_signup_with_existing_username(self):
         """Test for user signup with existing username"""
         with self.client as c:
-            headers = {"Content-type": self.mime_type}
             c.post(self.signup_url, json=self.signup_payload0,
-                   headers=headers)
+                   headers=self.headers)
             response = c.post(self.signup_url,
                               json=self.signup_payload0,
-                              headers=headers)
+                              headers=self.headers)
             result = json.loads(response.data.decode('utf-8'))
 
             self.assertEqual(result['error'], "That username already exists!")
@@ -33,14 +27,13 @@ class TestUsers(BaseTest):
     def test_signup_with_existing_email(self):
         """Test for user signup with existing email"""
         with self.client as c:
-            headers = {"Content-type": self.mime_type}
             c.post(self.signup_url, json=self.signup_payload0,
-                   headers=headers)
+                   headers=self.headers)
             self.signup_payload0['username'] = 'new_user'
             new_payload = self.signup_payload0
             response = c.post(self.signup_url,
                               json=new_payload,
-                              headers=headers)
+                              headers=self.headers)
             result = json.loads(response.data.decode('utf-8'))
 
             self.assertEqual(
@@ -50,14 +43,13 @@ class TestUsers(BaseTest):
     def test_signup_with_wrong_email(self):
         """Test for user signup with wrong email format"""
         with self.client as c:
-            headers = {"Content-type": self.mime_type}
             c.post(self.signup_url, json=self.signup_payload0,
-                   headers=headers)
+                   headers=self.headers)
             self.signup_payload0['email'] = '...@gmail.com'
             new_payload = self.signup_payload0
             response = c.post(self.signup_url,
                               json=new_payload,
-                              headers=headers)
+                              headers=self.headers)
             result = json.loads(response.data.decode('utf-8'))
 
             self.assertEqual(
@@ -67,15 +59,67 @@ class TestUsers(BaseTest):
     def test_with_missing_field(self):
         """Test for user signup with a missing field"""
         with self.client as c:
-            headers = {"Content-type": self.mime_type}
             c.post(self.signup_url, json=self.signup_payload0,
-                   headers=headers)
+                   headers=self.headers)
             self.signup_payload0['firstname'] = ''
             new_payload = self.signup_payload0
             response = c.post(self.signup_url,
                               json=new_payload,
-                              headers=headers)
+                              headers=self.headers)
             result = json.loads(response.data.decode('utf-8'))
 
-            self.assertEqual(
-                result['error'], "firstname is missing.")
+            self.assertEqual(result['error'], "firstname is missing.")
+
+    def test_login(self):
+        result = self.login()
+
+        self.assertEqual(result['message'], "Logged in successfully!")
+
+    def test_login_with_non_existent_user(self):
+        with self.client as c:
+            wrong_login_payload = {
+                "email": "nonexistent@user.com",
+                "password": "password"
+            }
+            response = c.post(self.signin_url,
+                              json=wrong_login_payload,
+                              headers=self.headers)
+            result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(
+            result['error'], "No user found with the given credentials")
+
+    def test_login_with_wrong_password(self):
+        self.sign_up()
+        with self.client as c:
+            wrong_login_password = {
+                "email": self.signin_payload0.get('email'),
+                "password": "wrongpassword"
+            }
+            response = c.post(self.signin_url,
+                              json=wrong_login_password,
+                              headers=self.headers)
+            result = json.loads(response.data.decode('utf-8'))
+        print(wrong_login_password['email'])
+        self.assertEqual(
+            result['error'], "Email/Password is invalid. Please check your"
+            " credentials")
+
+    def test_login_invalid_email(self):
+        self.sign_up()
+        with self.client as c:
+            wrong_email_format = {
+                "email": "...@gmail.com",
+                "password": "password"
+            }
+            response = c.post(self.signin_url,
+                              json=wrong_email_format,
+                              headers=self.headers)
+            result = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(
+            result['error'], "Email invalid")
+
+    def test_token_generation_in_login(self):
+        self.sign_up()
+        result = self.login()
+
+        self.assertIn("token", result)
