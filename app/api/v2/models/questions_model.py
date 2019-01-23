782470cf323113f05e24433fc363ce3ee249d00f
Joe Waithaka <waitha28ka@gmail.com>
2019-01-23 17:28:17 +0300

    [Feature #163416289] Write questions model

--- app/api/v2/models/questions_model.py
+++ app/api/v2/models/questions_model.py
@@ -1,11 +1,33 @@
 """ Question Model v2."""
-from app.api.v1.models.base_model import BaseModel
+from app.api.v2.models.base_model import BaseModel
+from app.db import database_transactions
 
 
 class QuestionModel(BaseModel):
-    """Question model Tests class."""
+    """Question model class."""
 
-    def __init__(self):
-        """Initialize the Meetup model."""
-
+    def __init__(self, title, body, meetup, user):
+        """Initialize the question instance"""
         super(QuestionModel, self).__init__()
+
+        self.title = title
+        self.body = body
+        self.meetup = meetup
+        self.user = user
+
+    def save(self):
+        """Save a new question to the database"""
+        sql = """
+        INSERT INTO meetup_questions (meetup_id,
+        user_id, question_title, question_body, question_votes)
+        VALUES ('{}','{}','{}','{}','{}')
+        """.format(self.meetup, self.user, self.title, self.body, 0)
+
+        database_transactions(sql)
+        data = {
+            "topic": self.title,
+            "body": self.body,
+            "meetup": self.meetup,
+            "user": self.user
+        }
+        return data
