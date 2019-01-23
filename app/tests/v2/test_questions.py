423787dab7cab923cf5fd9ba86bb20d098e9930d
Joe Waithaka <waitha28ka@gmail.com>
2019-01-24 02:04:31 +0300

    [Start #163432525] Write test for gettinga a specific quuestion record

--- app/tests/v2/test_questions.py
+++ app/tests/v2/test_questions.py
@@ -88,3 +88,89 @@ class TestQuestions(BaseTest):
             self.assertEqual(result['error'],
                              "That meetup for the meetup id passed is "
                              "currently not available")
+
+    def test_get_question(self):
+        """Test for getting a specific question record"""
+        # signup a test user
+        signup_response = self.sign_up()
+
+        # log the user in
+        login_response = self.login()
+
+        # create a test meetup
+        result = self.create_meetup()
+        meetup_id = result['data']['id']
+
+        self.headers.update({"x-access-token":
+                             login_response['token']})
+        question_payload = {
+            "title": "How to train your dragon?",
+            "body": "How does one get to tame their dragons?"
+                    " Mine just ate my neighbors",
+            "meetup": meetup_id
+        }
+        print(signup_response['data']['id'])
+
+        # create a test question
+        with self.client as c:
+            question_post_response = c.post('api/v2/questions',
+                                            json=question_payload,
+                                            headers=self.headers)
+            result = json.loads(question_post_response.data.decode('utf-8'))
+
+            self.assertEqual(result['message'],
+                             "Question posted to meetup successfully!")
+
+        # Get the newly created question
+        with self.client as c:
+            question_get_response = c.get('/api/v2/questions/1',
+                                          headers=self.headers)
+            result = json.loads(question_get_response.data.decode('utf-8'))
+
+            self.assertIn("data", result)
+            self.assertEqual(question_get_response.status_code, 200)
+
+    def test_get_question_of_non_existent_question(self):
+        """
+        Test for getting a specific question record that does not exist
+        """
+        # signup a test user
+        signup_response = self.sign_up()
+
+        # log the user in
+        login_response = self.login()
+
+        # create a test meetup
+        result = self.create_meetup()
+        meetup_id = result['data']['id']
+
+        self.headers.update({"x-access-token":
+                             login_response['token']})
+        question_payload = {
+            "title": "How to train your dragon?",
+            "body": "How does one get to tame their dragons?"
+                    " Mine just ate my neighbors",
+            "meetup": meetup_id
+        }
+        print(signup_response['data']['id'])
+
+        # create a test question
+        with self.client as c:
+            question_post_response = c.post('api/v2/questions',
+                                            json=question_payload,
+                                            headers=self.headers)
+            result = json.loads(question_post_response.data.decode('utf-8'))
+
+            self.assertEqual(result['message'],
+                             "Question posted to meetup successfully!")
+
+        # Get the newly created question
+        with self.client as c:
+            question_get_response = c.get('/api/v2/questions/12',
+                                          headers=self.headers)
+            result = json.loads(question_get_response.data.decode('utf-8'))
+
+            self.assertIn("error", result)
+            self.assertEqual(question_get_response.status_code, 404)
+            self.assertEqual(result['error'],
+                             "Could't find a question record with that id")
