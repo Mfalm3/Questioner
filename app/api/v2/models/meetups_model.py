a49bb52f3b4964c9195d87b9d708a6ed20a9b722
Joe Waithaka <waitha28ka@gmail.com>
2019-01-23 05:31:02 +0300

    [Feature #163388735] Create a meetup model

--- app/api/v2/models/meetups_model.py
+++ app/api/v2/models/meetups_model.py
@@ -1,10 +1,40 @@
 """ Meetups Model file."""
+import datetime
+import json
+from app.api.v2.utils.utils import datetime_serializer
 from app.api.v2.models.base_model import BaseModel
+from app.db import database_transactions
+
+time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %I:%M%P ")
 
 
 class MeetupsModel(BaseModel):
     """Meetups Model."""
 
-    def __init__(self):
+    def __init__(self, user_id, topic, location, happening_on, tags):
         """Initialize the Meetup model."""
-        super(MeetupsModel, self).__init__()
+        self.topic = topic
+        self.location = location
+        self.happening_on = happening_on
+        self.tags = tags
+        self.user_id = user_id
+        self.created_at = json.dumps(time_stamp,
+                                     default=datetime_serializer)
+
+    def save(self):
+        """Save meetup method"""
+        sql = """
+        INSERT INTO meetups (user_id, meetup_topic, meetup_location,
+        meetup_date, meetup_tags, created_at)
+        VALUES ( '{}', '{}', '{}', '{}', '{}', '{}' );
+        """.format(self.user_id, self.topic, self.location, self.happening_on,
+                   self.tags, self.created_at)
+        database_transactions(sql)
+
+        data = {
+            "topic": self.topic,
+            "location": self.location,
+            "happening_on": self.happening_on,
+            "tags": self.tags
+        }
+        return data
