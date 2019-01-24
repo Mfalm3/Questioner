3a16976ac12788ed95ce156c3439e6c872954264
Joe Waithaka <waitha28ka@gmail.com>
2019-01-24 03:10:18 +0300

    [Feature #163442110] Initialize voting test class

---
+++ app/tests/v2/test_voting.py
@@ -0,0 +1,10 @@
+"""Test for Voting"""
+import json
+from app.tests.v2.base_test import BaseTest
+
+
+class TestVoting(BaseTest):
+    """Voting Test class"""
+    def test_upvote_a_question(self):
+        """Test for upvoting a question"""
+        pass
