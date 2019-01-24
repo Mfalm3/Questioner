4202f31474f3208ef5811d83d30934f4f05ebcaf
Joe Waithaka <waitha28ka@gmail.com>
2019-01-24 05:51:14 +0300

    [Start #163442147] Write tests for downvote

--- app/tests/v2/test_voting.py
+++ app/tests/v2/test_voting.py
@@ -22,6 +22,7 @@ class TestVoting(BaseTest):
                              "Question upvoted successfully!")
 
     def test_upvote_continously(self):
+        """Test for continuous voting"""
         result = self.post_question()
         header = {"x-access-token": result[1]}
 
@@ -31,6 +32,37 @@ class TestVoting(BaseTest):
                                       headers=header)
 
             result = json.loads(upvote_response.data.decode('utf-8'))
+            print(result)
+
+            self.assertEqual(result['error'],
+                             "You can only vote once!")
+
+    def test_downvote_a_question(self):
+        """Test for upvoting a question"""
+        result = self.post_question()
+        header = {"x-access-token": result[1]}
+
+        with self.client as c:
+            upvote_response = c.patch('api/v2/questions/1/downvote',
+                                      headers=header)
+
+            result = json.loads(upvote_response.data.decode('utf-8'))
+            print(result)
+
+            self.assertEqual(result['message'],
+                             "Question downvoted successfully!")
+
+    def test_downvote_continously(self):
+        """Test for continuous voting"""
+        result = self.post_question()
+        header = {"x-access-token": result[1]}
+
+        with self.client as c:
+            c.patch('api/v2/questions/1/downvote', headers=header)
+            downvote_response = c.patch('api/v2/questions/1/downvote',
+                                        headers=header)
+
+            result = json.loads(downvote_response.data.decode('utf-8'))
             print(result)
 
             self.assertEqual(result['error'],
