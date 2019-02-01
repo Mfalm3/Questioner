1f0093545dddeac86a28d09a16e20c97f97890d2
Joe Waithaka <waitha28ka@gmail.com>
2019-02-01 12:59:15 +0300

    [Feature #163450540]Add test for rsvp meetup

--- app/tests/v2/test_meetups.py
+++ app/tests/v2/test_meetups.py
@@ -45,7 +45,6 @@ class TestMeetups(BaseTest):
         self.assertEqual(result['error'], "Action requires admin privilidges!")
         self.assertEqual(response.status_code, 403)
 
-
     def test_create_meetup_missing_field(self):
         """Test for creating missing field"""
         self.sign_up()
@@ -71,6 +70,7 @@ class TestMeetups(BaseTest):
         self.assertEqual(response.status_code, 400)
 
     def test_get_upcoming_meetups(self):
+        """Test for getting upcoming meetups"""
         with self.client as c:
             response = c.get('/api/v2/meetups/upcoming', headers=self.headers)
             result = json.loads(response.data.decode('utf-8'))
@@ -79,6 +79,7 @@ class TestMeetups(BaseTest):
             self.assertIsInstance(result['data'], list)
 
     def test_delete_meetup(self):
+        """Tests for deleting a meetup"""
         # signup a test user
         self.sign_up()
 
@@ -98,6 +99,7 @@ class TestMeetups(BaseTest):
             self.assertEqual(result['message'], "Meetup deleted successfully!")
 
     def test_get_meetup(self):
+        """Test for gettinga a meetup"""
         # signup a test user
         self.sign_up()
 
@@ -105,7 +107,7 @@ class TestMeetups(BaseTest):
         login_response = self.login()
 
         # create a test meetup
-        result = self.create_meetup()
+        self.create_meetup()
 
         self.headers.update({"x-access-token":
                              login_response['token']})
@@ -116,6 +118,7 @@ class TestMeetups(BaseTest):
             self.assertEqual(get_response.status_code, 200)
 
     def test_get_non_existent_meetup(self):
+        """Test for getting a meetup that does not exist"""
         # signup a test user
         self.sign_up()
 
@@ -136,6 +139,8 @@ class TestMeetups(BaseTest):
                              " doesn't exist")
 
     def test_get_with_alpha(self):
+        """Test for getting a meetup with an alpha character
+        passed in the url"""
         # signup a test user
         self.sign_up()
 
@@ -154,3 +159,20 @@ class TestMeetups(BaseTest):
             self.assertEqual(get_response.status_code, 400)
             self.assertEqual(result['error'], "The url requires only"
                              " digits for the id!")
+
+    def test_rsvp(self):
+        """Test for rsvp to a meetup"""
+        login_resp = self.login()
+        result = self.create_meetup()
+
+        self.headers.update({"x-access-token": login_resp['token']})
+        rsvp_payload = {"response": "yes"}
+        with self.client as c:
+            rsvp_response = c.post('api/v2/meetups/1/rsvps',
+                                   headers=self.headers,
+                                   json=rsvp_payload)
+            result = json.loads(rsvp_response.data.decode('utf-8'))
+
+            self.assertEqual(rsvp_response.status_code, 201)
+            self.assertEqual(result['status'], 201)
+            self.assertEqual(result['data'][0]['status'], "yes")
